4174e193011e50f1917d126328b15a138970ceaa
Pedro <pedronasserog@gmail.com>
2023-11-01 08:43:23 -0300

    merge main

--- src/main.c
+++ src/main.c
@@ -5,12 +5,8 @@
 #include "../inc/estoque.h"
 #include "../inc/login.h"
 
-<<<<<<< HEAD
-int main() {
-=======
 int main()
 {
   login();
->>>>>>> 4a837b6903139367dda0b6cc7d3f1d56ba5e1e9f
   return 0;
 }
