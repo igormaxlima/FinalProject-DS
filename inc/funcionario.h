e8875781997fcccc945e91df6329a98bd70c38e3
Pedro <pedronasserog@gmail.com>
2023-11-03 13:21:04 -0300

    colocando a struct do funcionario

--- inc/funcionario.h
+++ inc/funcionario.h
@@ -2,5 +2,10 @@
 #define FUNCIONARIO_H
 
 // DECLARACOES DAS FUNCOES SOBRE O MENU DE FUNCIONARIO
+typedef struct {
+    char id[10];
+    char username[51];
+    char password[16];
+} Employee;
 
 #endif
